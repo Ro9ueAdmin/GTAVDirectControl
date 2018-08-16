d7a44e8da2a2b1562acc88b41985dbac0bb12abb
E66666666 <emily66666666ok@outlook.com>
2018-08-16 22:02:09 +0200

    Implement proper countersteering

--- DirectControl/script.cpp
+++ DirectControl/script.cpp
@@ -3,8 +3,6 @@
 #include "Util/Logger.hpp"
 #include "Util/UIUtils.h"
 #include "Util/MathExt.h"
-#define _USE_MATH_DEFINES
-#include <math.h>
 
 Player player;
 Ped playerPed;
@@ -41,8 +39,7 @@ float CalculateReduction(Vehicle vehicle) {
     return mult;
 }
 
-// Despite being scientifically inaccurate, "self-aligning torque" is the best description.
-void drawSteeringLines(Vehicle vehicle, float steeringAngle) {
+void drawSteeringLines(Vehicle vehicle, float steeringAngle, float nextAngle) {
     Vector3 velocityWorld = ENTITY::GET_ENTITY_VELOCITY(vehicle);
     Vector3 positionWorld = ENTITY::GET_ENTITY_COORDS(vehicle, 1);
     Vector3 travelWorld = velocityWorld + positionWorld;
@@ -59,62 +56,64 @@ void drawSteeringLines(Vehicle vehicle, float steeringAngle) {
     float steeringAngleRelY = ENTITY::GET_ENTITY_SPEED(vehicle)*cos(steeringAngle);
     Vector3 steeringWorld = ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(vehicle, steeringAngleRelX, steeringAngleRelY, 0.0f);
 
+    float nextRelX = ENTITY::GET_ENTITY_SPEED(vehicle)*-sin(nextAngle);
+    float nextRelY = ENTITY::GET_ENTITY_SPEED(vehicle)*cos(nextAngle);
+    Vector3 nextWorld = ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(vehicle, nextRelX, nextRelY, 0.0f);
+
     GRAPHICS::DRAW_LINE(positionWorld.x, positionWorld.y, positionWorld.z, travelWorld.x, travelWorld.y, travelWorld.z, 0, 255, 0, 255);
     GRAPHICS::DRAW_LINE(positionWorld.x, positionWorld.y, positionWorld.z, turnWorldNorm.x, turnWorldNorm.y, turnWorldNorm.z, 255, 0, 0, 255);
     GRAPHICS::DRAW_LINE(positionWorld.x, positionWorld.y, positionWorld.z, steeringWorld.x, steeringWorld.y, steeringWorld.z, 255, 0, 255, 255);
+    GRAPHICS::DRAW_LINE(positionWorld.x, positionWorld.y, positionWorld.z, nextWorld.x, nextWorld.y, nextWorld.z, 255, 255, 255, 255);
+
 }
 
+float CalculateDesiredHeading(Vehicle vehicle, float steeringAngle, float steeringMax, float desiredHeading, float reduction) {
+    float correction = desiredHeading * reduction;
 
-float CalculateDesiredHeading(Vehicle vehicle, float steeringAngle, float steeringMax, float desiredHeading) {
-    Vector3 velocityWorld = ENTITY::GET_ENTITY_VELOCITY(vehicle);
-    Vector3 positionWorld = ENTITY::GET_ENTITY_COORDS(vehicle, 1);
-    Vector3 travelWorld = velocityWorld + positionWorld;
-    Vector3 travelRelative = ENTITY::GET_OFFSET_FROM_ENTITY_GIVEN_WORLD_COORDS(vehicle, travelWorld.x, travelWorld.y, travelWorld.z);
+    if (abs(ENTITY::GET_ENTITY_SPEED_VECTOR(vehicle, true).y) > 3.0f) {
+        Vector3 velocityWorld = ENTITY::GET_ENTITY_VELOCITY(vehicle);
+        Vector3 positionWorld = ENTITY::GET_ENTITY_COORDS(vehicle, 1);
+        Vector3 travelWorld = velocityWorld + positionWorld;
 
-    float steeringAngleRelX = ENTITY::GET_ENTITY_SPEED(vehicle)*-sin(steeringAngle);
-    float steeringAngleRelY = ENTITY::GET_ENTITY_SPEED(vehicle)*cos(steeringAngle);
-    Vector3 steeringWorld = ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(vehicle, steeringAngleRelX, steeringAngleRelY, 0.0f);
-    Vector3 steeringRelative = ENTITY::GET_OFFSET_FROM_ENTITY_GIVEN_WORLD_COORDS(vehicle, steeringWorld.x, steeringWorld.y, steeringWorld.z);
+        float steeringAngleRelX = ENTITY::GET_ENTITY_SPEED_VECTOR(vehicle, true).y*-sin(steeringAngle);
+        float steeringAngleRelY = ENTITY::GET_ENTITY_SPEED_VECTOR(vehicle, true).y*cos(steeringAngle);
+        Vector3 steeringWorld = ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(vehicle, steeringAngleRelX, steeringAngleRelY, 0.0f);
 
-    drawSteeringLines(vehicle, steeringAngle);
+        Vector3 travelNorm = Normalize(travelWorld - positionWorld);
+        Vector3 steerNorm = Normalize(steeringWorld - positionWorld);
+        float travelDir = atan2(travelNorm.y, travelNorm.x) + desiredHeading;
+        float steerDir = atan2(steerNorm.y, steerNorm.x);
 
-    Vector3 travelNorm = Normalize(travelWorld - positionWorld);
-    Vector3 steerNorm = Normalize(steeringWorld - positionWorld);
-    float travelDir = atan2(travelNorm.y, travelNorm.x);// *(180.0f / 3.14159f);
-    float steerDir = atan2(steerNorm.y, steerNorm.x);// *(180.0f / 3.14159f);
+        correction = 2.0f * atan2(sin(travelDir - steerDir), cos(travelDir - steerDir));
+    }
+    if (correction > steeringMax)
+        correction = steeringMax;
+    if (correction < -steeringMax)
+        correction = -steeringMax;
 
-    return atan2(sin(travelDir - steerDir), cos(travelDir - steerDir));
-    //return travelDir - steerDir;//GAMEPLAY::GET_ANGLE_BETWEEN_2D_VECTORS(travelWorld.x, travelWorld.y, steeringWorld.x, steeringWorld.y);
-    //return (steeringRelative.x - travelRelative.x);
+
+    return correction;
 }
 
 float getSteeringAngle(Vehicle v) {
+    float largestAngle = 0.0f;
     auto angles = ext.GetWheelSteeringAngles(v);
-    float wheelsSteered = 0.0f;
-    float avgAngle = 0.0f;
 
-    for (int i = 0; i < ext.GetNumWheels(v); i++) {
-        if (i < 3 && angles[i] != 0.0f) {
-            wheelsSteered += 1.0f;
-            avgAngle += angles[i];
+    for (auto angle : angles) {
+        if (abs(angle) > abs(largestAngle)) {
+            largestAngle = angle;
         }
     }
-
-    if (wheelsSteered > 0.5f && wheelsSteered < 2.5f) { // bikes, cars, quads
-        avgAngle /= wheelsSteered;
-    }
-    else {
-        avgAngle = ext.GetSteeringAngle(v)*ext.GetSteeringMultiplier(v); // tank, forklift
-    }
-    return avgAngle;
+    return largestAngle;
 }
 
 void UpdateControl() {
-    float ffbSteer = 0;
+    float desiredHeading = 0;
 
     if (!VEHICLE::GET_IS_VEHICLE_ENGINE_RUNNING(vehicleToControl))
         VEHICLE::SET_VEHICLE_ENGINE_ON(vehicleToControl, true, true, true);
 
+    float actualAngle = getSteeringAngle(vehicleToControl);
     float limitRadians = ext.GetMaxSteeringAngle(vehicleToControl);
     float reduction = CalculateReduction(vehicleToControl);
     bool handbrake = GetAsyncKeyState('O') & 0x8000 ? 1.0f : 0.0f;
@@ -125,27 +124,26 @@ void UpdateControl() {
         throttle = -1.0f;
 
     float brake = GetAsyncKeyState('K') & 0x8000 ? 1.0f : 0.0f;
-    float left = GetAsyncKeyState('J') & 0x8000 ? 1.0f : 0.0f;
-    float right = GetAsyncKeyState('L') & 0x8000 ? 1.0f : 0.0f;
+    float left = GetAsyncKeyState('J') & 0x8000 ? limitRadians : 0.0f;
+    float right = GetAsyncKeyState('L') & 0x8000 ? limitRadians : 0.0f;
     float steer = left;
     if (right > 0.5f) steer = -right;
 
-    steer = reduction * steer * limitRadians;
-
-    ffbSteer = CalculateDesiredHeading(vehicleToControl, getSteeringAngle(vehicleToControl), limitRadians, steer);
+    desiredHeading = CalculateDesiredHeading(vehicleToControl, actualAngle, limitRadians, steer, reduction);
     ext.SetThrottleP(vehicleToControl, throttle);
     ext.SetBrakeP(vehicleToControl, brake);
-    ext.SetSteeringAngle(vehicleToControl, steer == 0.0f ? ffbSteer : steer);
+    
+    ext.SetSteeringAngle(vehicleToControl, lerp(actualAngle, desiredHeading, 20.0f * GAMEPLAY::GET_FRAME_TIME()));
     ext.SetHandbrake(vehicleToControl, handbrake);
 
     showText(0.1, 0.05, 0.5, "T: " + std::to_string(throttle));
     showText(0.1, 0.10, 0.5, "B: " + std::to_string(brake));
     showText(0.1, 0.15, 0.5, "S: " + std::to_string(steer));
     showText(0.1, 0.20, 0.5, "H: " + std::to_string(handbrake));
-    showText(0.1, 0.25, 0.5, "FS: " + std::to_string(ffbSteer));
-    showText(0.1, 0.30, 0.5, "AS: " + std::to_string(getSteeringAngle(vehicleToControl)));
+    showText(0.1, 0.25, 0.5, "FS: " + std::to_string(desiredHeading));
+    showText(0.1, 0.30, 0.5, "AS: " + std::to_string(actualAngle));
 
-
+    drawSteeringLines(vehicleToControl, actualAngle, desiredHeading);
 }
 
 void update()
